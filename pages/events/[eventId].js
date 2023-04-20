98d598f65d421e86f4416c7d906d177cdabfbc5b
Elvan Hidayat <elvanhd@gmail.com>
2023-04-20 22:59:53 +0700

    adding dynamic head content

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment } from 'react';
 
 import EventContent from '../../components/event-detail/event-content';
@@ -18,6 +19,10 @@ function EventDetailPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name='description' content={event.description} />
+      </Head>
       <EventSummary title={event.title} />
       <EventLogistics
         date={event.date}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 
@@ -17,6 +18,16 @@ function AllEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>All my events</title>
+      </Head>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
