b575976590a879e88d9a9b11466f23dd96a847a8
Elvan Hidayat <elvanhd@gmail.com>
2023-05-10 22:50:17 +0700

    displaying comments

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { Fragment } from 'react';
 import EventContent from '../../components/event-detail/event-content';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventSummary from '../../components/event-detail/event-summary';
+import Comments from '../../components/input/comments';
 import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 
 function EventDetailPage(props) {
@@ -33,6 +34,7 @@ function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
